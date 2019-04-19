536a8795e5e974128de181d9ba186e67e1d44a46
Marius Andrei Brinzaru <MariusB@Ortec.Com>
2019-04-19 14:43:50 +0300

    dam drumu la treaba

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL & E_STRICT);
+ini_set('display_errors', '1');
+ini_set('log_errors', '1');
+ini_set('error_log', './');
+$con = mysqli_connect("localhost","root","","hotel") or die(mysql_error());
+
+?>
